b2630826ba9f594b58ed63c5d674cc7e3341951f
Pedro <pedro.branco99@hotmail.com>
2026-08-06 00:32:47 -0300

    correção da ordem de inicializacao do asgi para o daphne

--- onematch/asgi.py
+++ onematch/asgi.py
@@ -1,14 +1,16 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import apps.chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onematch.settings')
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from apps.chat.middleware import JWTAuthMiddlewareStack
+import apps.chat.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "http": django_asgi_app,
+    "websocket": JWTAuthMiddlewareStack(
         URLRouter(
             apps.chat.routing.websocket_urlpatterns
         )
